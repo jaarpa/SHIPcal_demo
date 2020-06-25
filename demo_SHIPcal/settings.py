dc7f703d468f0a27306038eb3ece53ccea4c3847
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2020-06-25 11:36:44 -0500

    started the translation

--- demo_SHIPcal/settings.py
+++ demo_SHIPcal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -106,6 +107,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
+
+LANGUAGES = [
+  ('es', _('Spanish')),
+  ('en', _('English')),
+]
 
 LANGUAGE_CODE = 'en-us'
 
